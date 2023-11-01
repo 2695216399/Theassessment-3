bed74874e4f9cbd91f4c805774eb31d3412fa1b2
2695216399 <yuanqing.wang-1@student.uts.edu.au>
2023-11-01 18:38:40 +1100

    Score and time

--- Assessment-3(Game)/Assets/Script/UIManager.cs
+++ Assessment-3(Game)/Assets/Script/UIManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text highScoreText;
+    public Text gameTimeText;
+
+    int newScore = 5000;
+    float newGameTime = 150.0f;
+
     void Start()
     {
-        
+        LoadPlayerPrefs();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        int oldHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (newScore > oldHighScore)
+        {
+            
+            PlayerPrefs.SetInt("HighScore", newScore);
+            SavePlayerPrefs(); 
+        }
+
+        float oldGameTime = PlayerPrefs.GetFloat("GameTime", 0.0f);
+        if (newGameTime > oldGameTime)
+        {
+            
+            PlayerPrefs.SetFloat("GameTime", newGameTime);
+            SavePlayerPrefs();
+        }
+    }
+
+    void LoadPlayerPrefs()
+    {
+        int loadedHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        float loadedGameTime = PlayerPrefs.GetFloat("GameTime", 0.0f);
+
+        highScoreText.text = "High Score: " + loadedHighScore.ToString();
+        gameTimeText.text = "Game Time: " + loadedGameTime.ToString("F2");
+    }
+
+    void SavePlayerPrefs()
+    {
+        PlayerPrefs.Save();
     }
 }
